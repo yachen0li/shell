e797437e5ee8e7f4f8a3bb7beb1e859a7d15cdb9
Yachen Li <yachenli07-demo@users.noreply.github.com>
2025-11-06 21:24:42 -0500

    assigment_final

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -51,9 +51,7 @@ cp ./data/raw/*event*.log ./data/processed/event_logs
 <<<<<<< HEAD
 rm ./data/raw/*ipaddr* 
 rm ./data/processed/user_logs/*ipaddr*
-
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 find ./data/processed -type f > ./data/inventory.txt
-
 ###########################################
 echo "Project setup is complete!"
